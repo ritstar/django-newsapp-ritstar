f41d55db0401e9e1803a8c61d4212831894c0f07
ritstar <riteshjha0270@gmail.com>
2018-06-30 23:03:21 +0530

    News Api App

--- ritesh/views.py
+++ ritesh/views.py
@@ -7,11 +7,11 @@ from newsapi import NewsApiClient
 # Create your views here.
 def index(request):
     url = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=23f1ea61ddf444d7b98b68f746262f26';
-    req = requests.get(url);
-    res = json.loads(req.text);
+    google_url = 'https://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey=23f1ea61ddf444d7b98b68f746262f26'
+    
     newsapi = NewsApiClient(api_key='23f1ea61ddf444d7b98b68f746262f26')
-    top_headlines = newsapi.get_top_headlines(q='bitcoin',category='business',language='en')
-    all_articles = newsapi.get_everything(q='bitcoin',sources='bbc-news,the-verge',domains='bbc.co.uk,techcrunch.com',from_param='2017-12-01',to='2017-12-12',language='en',sort_by='relevancy',page=2)
+    top_headlines = newsapi.get_top_headlines(category='technology',language='en',country='in')
+    all_articles = newsapi.get_everything(sources='bbc-news,the-verge,abc-news,crypto coins news',language='en',sort_by='relevancy',page=2,)
 
     ind_dic = { 'articles': all_articles, 'headl' : top_headlines}
 
