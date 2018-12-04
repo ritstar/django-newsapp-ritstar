953ededd7abd9bd8c6a06a1af6c0e2ac35dabc32
Ritesh Jha (RitstaR) <riteshjha0270@gmail.com>
2018-12-04 22:28:03 +0530

    Update settings.py

--- dblog/settings.py
+++ dblog/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = []
 #For SMTP Email
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'riteshjha02700@gmail.com'
-EMAIL_HOST_PASSWORD = '18Azerty.pad'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 # Application definition
 
